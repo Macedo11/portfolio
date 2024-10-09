14cfc999c42d4b94828c1e29ffd2b892cb5ba97c
João Victor da Silva Macedo <123326038+Macedo11@users.noreply.github.com>
2024-10-09 14:06:20 -0300

    Configurando mqs

--- scripts.js
+++ scripts.js
@@ -2,10 +2,12 @@ var hidden = document.querySelector('#hidden');
 var btn = document.querySelector('#btn_projetos');
 
 btn.addEventListener('click', function() {
-    if(hidden.style.display === 'block') {
-        hidden.style.display = 'none'
+
+    if(window.getComputedStyle(hidden).display === 'none') {
+        hidden.style.display = 'block'
+        btn.textContent = 'Veja menos'
     } else {
-        hidden.style.display ='block'
-        btn.innerHTML = 'Veja menos'
+        hidden.style.display = 'none'
+        btn.textContent = 'Veja mais'
     }
 })
